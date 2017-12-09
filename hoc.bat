8a8ca909033470147de3677cd21aa10f7e2cfda1
icm icm <icm@fer.hr>
2017-12-09 20:42:35 +0100

    updejto skriptu da radi poso mjesto nas...

--- hoc.bat
+++ hoc.bat
@@ -2,3 +2,4 @@ C:\ProgramData\Anaconda3\Scripts\pip.exe install pyqtgraph
 C:\ProgramData\Anaconda3\Scripts\pip.exe install serial
 C:\ProgramData\Anaconda3\Scripts\pip.exe install pyserial
 git clone https://github.com/ihrabar/ICM-HOC-2017.git
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Spyder.lnk"
