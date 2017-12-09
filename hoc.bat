3b4d54278cc952ce28f4918e8fb7e57587c7e76a
icm icm <icm@fer.hr>
2017-12-09 20:24:01 +0100

    dodana korisna skripta za radionice, ovo nemoj pobrisat...

---
+++ hoc.bat
@@ -0,0 +1,4 @@
+C:\ProgramData\Anaconda3\Scripts\pip.exe install pyqtgraph
+C:\ProgramData\Anaconda3\Scripts\pip.exe install serial
+C:\ProgramData\Anaconda3\Scripts\pip.exe install pyserial
+git clone https://github.com/ihrabar/ICM-HOC-2017.git
