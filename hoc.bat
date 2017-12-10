e06a62fe6c1b5c3fef3cb3e5fc34e59add4c04c8
gopovic <goranpopovic@hotmail.com>
2017-12-10 02:05:05 +0100

    Dodavanje brisanja
    svih fileova i foldera osim radne_skripte u hoc.bat

--- hoc.bat
+++ hoc.bat
@@ -2,4 +2,10 @@ C:\ProgramData\Anaconda3\Scripts\pip.exe install pyqtgraph
 C:\ProgramData\Anaconda3\Scripts\pip.exe install serial
 C:\ProgramData\Anaconda3\Scripts\pip.exe install pyserial
 git clone https://github.com/ihrabar/ICM-HOC-2017.git
+rmdir /S /Q ICM-HOC-2017\.git
+rmdir /S /Q ICM-HOC-2017\radionica_01
+rmdir /S /Q ICM-HOC-2017\radionica_02
+del ICM-HOC-2017\README.md
+del ICM-HOC-2017\hoc.bat
+
 "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Spyder.lnk"
